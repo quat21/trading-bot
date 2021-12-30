4f9019fd4ba72079ecfc9bfa596e1011890ffca8
ta <hgiweods@gmx.de>
2021-12-30 13:35:31 +0100

    add logging

--- README.md
+++ README.md
@@ -5,10 +5,19 @@ Notes:
 - put API keys, secrets, and passwords in an authentication.json file in the project directory (using the format specified in authentication_example.json)
 
 TODO:
+- implement coinbase exchange
+- implement basic trading strategy
 - look into best way to do error handling
   - handling of connection errors
-  - handling of random runtime errors
-  - emergency stop procedure?
+  - handling of other runtime errors
+  - emergency stop procedure
   - logging of errors
+- check code with AWS CodeGuru
 - plan out dash app for control
 - implement dash app
+  - e-mail notification
+  - live view of performance over time
+  - selection of exchange and bot
+  - live view of price data
+  - live view trades
+- setup CI/CD pipeline to host on AWS
--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 '''Instantiate and start a trading bot.'''
 
+import logging
 import json
 import bots
 import exchanges
 
-file = open('authentication.json', 'r', encoding='UTF-8')
-authentication = json.load(file)
-file.close()
-print(authentication)
 
-'''
-exchange = exchanges.abstract_exchange.Exchange('', '', '')
+def get_authentication_data(exchange: str) -> dict:
+    '''Return authentcation data for a given exchange.'''
+    file = open('authentication.json', 'r', encoding='UTF-8')
+    authentication = json.load(file)
+    file.close()
+    if exchange in authentication.keys():
+        return authentication[exchange]
+    else:
+        return {}
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    filename='app.log',
+    filemode='w',
+)
+
+authentication = get_authentication_data('coinbase_pro_sandbox')
+exchange = exchanges.abstract_exchange.Exchange(authentication['key'],
+                                                authentication['secret'],
+                                                authentication['password'])
 bot = bots.abstract_bot.Bot(exchange)
 
 bot.start()
-'''
